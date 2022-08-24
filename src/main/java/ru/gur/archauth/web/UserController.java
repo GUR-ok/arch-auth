b7f7dc23e5fcd2a94922bcd9ff325d675816928d
Yuriy Gorelov <gur-ok@yandex.ru>
2022-08-24 20:44:45 +0400

    add endpoints for envoy filter

--- src/main/java/ru/gur/archauth/web/UserController.java
+++ src/main/java/ru/gur/archauth/web/UserController.java
@@ -1,17 +1,22 @@
 package ru.gur.archauth.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gur.archauth.service.AuthService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -38,5 +43,28 @@ public class UserController {
         response.setHeader("x-username", userDto.getLogin());
 
         return Collections.singletonMap("token", token);
+    }
+
+    /**
+     * Методы для envoy фильтра Istio. Ничего не делают, созданы для проверки работы фильтра.
+     * Для POST/GET/PATCH требуется создание своего маппинга.
+     * Тут можно добавить проверку сессии, валидации прихраненного токена и т.д.
+     */
+    @GetMapping(value = "/auth/istio/**")
+    public void istio(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Method: GET");
+
+        response.setHeader("x-auth-token", request.getHeader("x-auth-token"));
+
+        log.info("Token: " + request.getHeader("x-auth-token"));
+    }
+
+    @PatchMapping(value = "/auth/istio/**")
+    public void istioPatch(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Method: PATCH");
+
+        response.setHeader("x-auth-token", request.getHeader("x-auth-token"));
+
+        log.info("Token: " + request.getHeader("x-auth-token"));
     }
 }
